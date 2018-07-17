90e9385ea4f2cd9d019e0897a4c7d17e88dc99fa
kajate <patrick.gardinger@gmail.com>
2018-07-17 16:23:06 +0200

    changed stuff

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var app = express();
-var server = require("https").Server(app);
+var server = require("http").Server(app);
 var io = require("socket.io").listen(server);
 
 var players = {};
@@ -445,8 +445,8 @@ io.on("connection", function(socket) {
     });
 });
 
-server.listen(process.env.PORT || 8080);
+// server.listen(process.env.PORT || 8080);
 
-// server.listen(8081, function() {
-//     console.log(`Listening on ${server.address().port}`);
-// });
+server.listen(8081, function() {
+    console.log(`Listening on ${server.address().port}`);
+});
